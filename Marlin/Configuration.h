611ee2df8535f772a96eff9ef6fdf37c7709f68f
Zachary Tipnis <tipniz@rpi.edu>
2020-08-06 17:40:14 -0400

    Status screen with heated bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1003,11 +1003,11 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -10, 2 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -12, -4.5 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 10
+#define PROBING_MARGIN 1
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_SPEED (133*60)
